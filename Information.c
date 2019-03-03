a971a00d0901bb5db7f967df99a3db3cad931be2
samr <uhsamr2018@gmail.com>
2019-03-02 18:11:41 -0600

    adding comments and testing stuff

---
+++ Information.c
@@ -0,0 +1,74 @@
+/****************************************************************
+7-SEGMENT DISPLAY LETTERS:0000.0000.0000.0000.0000.0000.0XXX.XXXX
+						  0000.0000.0000.0000.0000.0000.0654.3210
+						  
+    WriteHex(HEX0,0x8);		A
+    WriteHex(HEX0,0x60);	b
+    WriteHex(HEX0,0x61);	C
+    WriteHex(HEX0,0x72);	c
+	WriteHex(HEX0,0x42);	d
+	WriteHex(HEX1,0x6);		E
+	WriteHex(HEX0,0x38);	F
+	WriteHex(HEX0,0x21);	G
+	WriteHex(HEX0,0x9);		H
+	WriteHex(HEX0,0x68);	h
+	WriteHex(HEX0,0x79);	I/1
+	WriteHex(HEX0,0x43);	J
+	WriteHex(HEX2,0x47);	L
+	WriteHex(HEX0,0x6A);	n
+	WriteHex(HEX4,0x40);	O/0
+	WriteHex(HEX0,0x62);	o
+	WriteHex(HEX0,0x18);	P
+	WriteHex(HEX0,0xC);		q
+	WriteHex(HEX0,0x7A);	r
+	WriteHex(HEX0,0x24);	S/5
+	WriteHex(HEX0,0x70);	t
+	WriteHex(HEX0,0x41);	U
+	WriteHex(HEX0,0x63);	u
+	WriteHex(HEX0,0x44);	y
+	WriteHex(HEX5,0x7F); 	OFF/8
+	WriteHex(HEX5,0x24);	2
+	WriteHex(HEX5,0x30);	3
+	WriteHex(HEX5,0x19);	4
+	WriteHex(HEX5,0x2);		6
+	WriteHex(HEX5,0x7);		7
+	WriteHex(HEX5,0x10);	9
+*****************************************************************/
+
+
+
+/****************************************************************
+LEDS:0000.0000.0000.0000.0000.00XX.XXXX.XXXX
+	 0000.0000.0000.0000.0000.0098.7654.3210
+	 
+0x0 = 00.0000.0000
+0x1 = 00.0000.0001
+0x2 = 00.0000.0010
+0x4 = 00.0000.0100
+0x8 = 00.0000.1000
+0x10 = 00.0001.0000
+0x20 = 00.0010.0000
+0x40 = 00.0100.0000
+0x80 = 00.1000.0000
+0x100 = 01.0000.0000
+0x200 = 10.0000.0000
+0x3FF = 11.1111.1111
+*****************************************************************/
+
+
+
+/****************************************************************
+KEYS:0000.0000.0000.0000.0000.0000.0000.XXXX
+	 0000.0000.0000.0000.0000.0000.0000.3210
+	 
+	0x0 = 00.0000.0000//No buttons pressed
+	0x1 = 00.0000.0001//KEY0 pressed
+	0x2 = 00.0000.0010//KEY1 pressed
+	0x4 = 00.0000.0100//KEY2 pressed
+	0x4 = 00.0000.0100//KEY3 pressed
+*****************************************************************/
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+//When writing to HEX, LED etc. look at them as bits:
+/* HEX# = bit-6 bit-5 bit-4 bit-3 bit-2 bit-1 bit-0 = 0000000 to turn them all on or 1111111 to turn them all off, 
+ but that is for binary, so we must write a hex number or decimal equivalent to the binary bits we want */
+/* LED = bit-9 bit-8 bit-7 bit-6 bit-5 bit-4 bit-3 bit-2 bit-1 bit-0 */
 #include "samr_driver.h"
 
 int main() {
@@ -6,67 +10,34 @@ int main() {
  unsigned int pw;
  int pwgoodl, pwgoodr;
 
+
  init();
-
- for(i = 0; i < 10;i++) {
-   WriteLed(i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX0,i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX1,i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX2,i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX3,i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX4,i);
-   usleep( 100*1000 );
- }
-
- for(i = 0; i < 7;i++) {
-   WriteHex(HEX5,i);
-   usleep( 100*1000 );
- }
-
-// for(;;)	{
-	 // ReadPW(PW0,&pw);
-	 // printf("PW reading in HEX: %x\r\n",pw);
-	 // printf("PW reading in int: %x\r\n",pw);
-// }
-//to activate PWM bit 32 must be a 1.
-	pwgoodl = WritePWM(PWMl,2147483648 + 10000);
-	pwgoodr = WritePWM(PWMr,2147483648 + 5000);	
 	
-	 //ReadPW(PW0,&pw);
-	printf("PWMl returned: %d\r\n",pwgoodl);
-	printf("PWMr returned: %d\r\n",pwgoodr);
-	  
-	if((pwgoodl == 0) && (pwgoodr == 0)){
-	printf("PWMr and PWMl are in Good State\r\n");
-	}
-
-
- // for(;;) {
-   // unsigned int sw = ReadSwitches();
-   // unsigned int keys = ReadKeys();
-   // printf("Switches: %08x, Keys: %08x \n",sw,keys);
-   // usleep( 100*1000 );
- // }
-
+	WriteLed(0x0);
+    usleep( 100*1000 );
+	
+	WriteHex(HEX0,0x9);
+    usleep( 100*1000 );
+	
+	WriteHex(HEX1,0x6);
+    usleep( 100*1000 );
+	
+	WriteHex(HEX2,0x47);
+    usleep( 100*1000 );
+   
+	WriteHex(HEX3,0x47);
+    usleep( 100*1000 );
+	
+	WriteHex(HEX4,0x40);
+    usleep( 100*1000 );
+	
+	WriteHex(HEX5,0x7F);
+    usleep( 100*1000 );
+	
+	
+    
+	for(;;){//turns on corresponding LED when switch is flipped on.
+	WriteLed(ReadSwitches());}
+	
 return( 0 );
 }
